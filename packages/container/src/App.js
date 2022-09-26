46366655d1755c1ad1f24543e11b2d17791e2ed3
suleman008cj <Mohammad2.Suleman@ril.com>
2022-09-27 00:51:22 +0530

    styles fixed

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,13 +2,19 @@ import React from 'react';
 import MarketingApp from './compoenents/MarketingApp';
 import Header from './compoenents/Header';
 import { BrowserRouter } from 'react-router-dom';
+import { StylesProvider, createGenerateClassName } from '@material-ui/styles';
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'co',
+});
 export default () => {
   return (
     <BrowserRouter>
-      <div>
-        <Header />
-        <MarketingApp />
-      </div>
+      <StylesProvider generateClassName={generateClassName}>
+        <div>
+          <Header />
+          <MarketingApp />
+        </div>
+      </StylesProvider>
     </BrowserRouter>
   );
 };
--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import { Switch, BrowserRouter, Route } from 'react-router-dom';
-import { StylesProvider } from '@material-ui/core/styles';
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from '@material-ui/core/styles';
 import Pricing from './components/Pricing';
 import Landing from './components/Landing';
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'ma',
+});
 
 export default () => {
   return (
     <div>
-      <StylesProvider>
+      <StylesProvider generateClassName={generateClassName}>
         <BrowserRouter>
           <Switch>
             <Route exact path="/pricing" component={Pricing} />
